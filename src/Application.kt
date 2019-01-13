00d8c25cc7eadade4c653765d90606a0efeb3e95
Hannes Dorfmann <hannes.dofmann@gmail.com>
2019-01-13 10:37:43 +0100

    Hello world

---
+++ src/Application.kt
@@ -0,0 +1,38 @@
+package com.checkify
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import freemarker.cache.*
+import io.ktor.freemarker.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        post("/accesstoken") {
+            call.respond(FreeMarkerContent("accesstoken.ftl", mapOf("accessToken" to "qwe")))
+        }
+    }
+}
